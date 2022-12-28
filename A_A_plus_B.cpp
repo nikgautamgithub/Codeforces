8a22ca59d3a29a12fd1609b928c10a49f26f6c56
Swastik Sharma <swastiksharmaaa09@gmail.com>
2022-12-29 01:32:07 +0530

    code

---
+++ A_A_plus_B.cpp
@@ -0,0 +1,53 @@
+#include<algorithm>
+#include<iostream>
+#include<iterator>
+#include<map>
+#include<vector>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<numeric>
+
+
+//Swastik Sharma
+#define f                       first
+#define s                       second
+#define ll                      long long
+#define pb                      push_back
+#define mp                      make_pair
+#define pii                     pair<int,int>
+#define vi                      vector<int>
+#define mii                     unordered_map<int,int>
+#define me(i,start,end)         for(int i =start; i<end; ++i)
+#define she(i,start,end)        for(int i =end-1; i>=start; --i)
+#define all(v)                  v.begin() , v.end()
+#define w(x)                    int x;cin>>x;while(x--)
+
+using namespace std;
+
+
+void solve()
+{
+    //your code goes here
+    
+    string s;
+    cin>>s;
+
+    int a = s[0] - 48;
+    int b = s[2] - 48;
+
+    cout<<a + b<<endl;
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);  
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }return 0;
+}
